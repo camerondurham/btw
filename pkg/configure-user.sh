4a137cb2a32a67070a7c9879eaad210512bd9a88
Cameron Durham <cdurham@usc.edu>
2021-08-02 01:27:49 -0700

    Cleanup install scripts, add proper terminal

--- pkg/configure-user.sh
+++ pkg/configure-user.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ "$EUID" -eq 0 ]
     then echo "do not run as root"
         exit
@@ -10,12 +9,10 @@ set -x
 
 XDG_CONFIG_DIR_LOCAL="$HOME/.config"
 
-
-echo "installing vim-plug"
-echo
+# install vim plug
 if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # symlink dotfiles into rightful location on machine
@@ -28,6 +25,9 @@ do
         ln -iv "${PWD}/${item}" "${HOME}/.${item##*/}"
     fi
 done
+
+# set default shell to zsh
+chsh -s /usr/bin/zsh
 
 # update xfce settings
 killall xfconfd
