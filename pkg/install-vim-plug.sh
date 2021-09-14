c4e70107b4e8889507e446546ccae6f8cd9be630
Cameron Durham <cdurham@usc.edu>
2021-09-13 22:16:56 -0700

    Add new packages

---
+++ pkg/install-vim-plug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "installing vim-plug"
+echo
+if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
