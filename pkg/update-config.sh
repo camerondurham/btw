b95ce8daa82a028184630781d3e3118fbb50beae
Cameron Durham <camrs@protonmail.com>
2021-09-25 19:10:54 -0700

    improve script

--- pkg/update-config.sh
+++ pkg/update-config.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
+function update() {
+
+	# copy configdirs/{aliases,nvim,tmux} to .config/
+	cp -r ~/.config/aliases ../config/configdirs/aliases 
+	cp -r ~/.config/nvim ../config/configdirs/nvim 
+	cp -r ~/.config/tmux ../config/configdirs/tmux 
+
+	# dotfiles and such to ~/.*
+	cp ~/.gitconfig ../config/dotfiles/gitconfig 
+	cp ~/.xbindkeysrc ../config/dotfiles/xbindkeysrc 
+	cp ~/.xinitrc ../config/dotfiles/xinitrc 
+	cp ~/.Xresources ../config/dotfiles/Xresources 
+	cp ~/.zprofile ../config/dotfiles/zprofile 
+	cp ~/.zshrc ../config/dotfiles/zshrc 
+
+}
+
 if [ "$EUID" -eq 0 ]
     then echo "do not run as root"
         exit
 fi
 
-set -x
+echo -n "Are you sure you want to copy files into setup? (y/n)"
+read answer
 
-echo "Are you sure you want to overwrite config in $HOME/setup?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes|y ) break;;
-        No|n ) exit;;
-    esac
-done
-
-
-
-# copy configdirs/{aliases,nvim,tmux} to .config/
-cp -r ~/.config/aliases ../config/configdirs/aliases 
-cp -r ~/.config/nvim ../config/configdirs/nvim 
-cp -r ~/.config/tmux ../config/configdirs/tmux 
-
-# dotfiles and such to ~/.*
-cp ~/.gitconfig ../config/dotfiles/gitconfig 
-cp ~/.xbindkeysrc ../config/dotfiles/xbindkeysrc 
-cp ~/.xinitrc ../config/dotfiles/xinitrc 
-cp ~/.Xresources ../config/dotfiles/Xresources 
-cp ~/.zprofile ../config/dotfiles/zprofile 
-cp ~/.zshrc ../config/dotfiles/zshrc 
-
+if [ "$answer" != "${answer#[Yy]}" ]; then
+	update
+else
+	echo "quitting..."
+	exit
+fi
