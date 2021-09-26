da64d9228ffc576a6749e320c35142eb009a3311
Cameron Durham <camrs@protonmail.com>
2021-09-26 01:53:29 -0700

    Add rules for oryx and only add rules for ergodox keyboard

--- pkg/configure-system.sh
+++ pkg/configure-system.sh
@@ -5,25 +5,41 @@ if [ "$EUID" -ne 0 ]
         exit
 fi
 
-printf "[main]\ndhcp=dhclient" > /etc/NetworkManager/conf.d/dhcp-client.conf
-systemctl enable NetworkManager
+# currently using systemd-networkd
+# printf "[main]\ndhcp=dhclient" > /etc/NetworkManager/conf.d/dhcp-client.conf
+# systemctl enable NetworkManager
 timedatectl set-ntp true
 
 ln -sf /usr/bin/nvim /usr/bin/vim
 
 localectl set-locale LANG=en_US.UTF-8
 
+# create udev rule for keyboard stuff
+# from https://github.com/zsa/wally/wiki/Linux-install
+sudo mkdir -p /etc/udev/rules.d/
 
+sudo touch /etc/udev/rules.d/50-wally.rules
+sudo cp ../config/udev/rules.d/50-wally.rules /etc/udev/rules.d/50-wally.rules
+
+sudo touch /etc/udev/rules.d/50-oryx.rules
+sudo cp ../config/udev/rules.d/50-oryx.rules /etc/udev/rules.d/50-oryx.rules
+
+sudo groupadd plugdev
+sudo usermod -aG plugdev $USER
+
+echo "remember to log out to use wally or training tool for changes to take effect"
+
+# TODO: fix this
 # make vconsole font larger
 # VCONSOLE_FONT=FONT=ter-v32n.psf.gz
 # touch /etc/vconsole.conf
 # grep -qxF ${VCONSOLE_FONT} /etc/vconsole.conf || sudo echo ${VCONSOLE_FONT} >> /etc/vconsole.conf
-# 
+#
 # # set grub font size
 # grub-mkfont -s 60 -o /boot/grubfont.pf2 /usr/share/fonts/TTF/Hack-Regular.ttf
 # GRUB_FONT=GRUB_FONT=\"/boot/grubfont.pf2\"
 # grep -qxF ${GRUB_FONT} /etc/default/grub || echo ${GRUB_FONT} >> /etc/default/grub
-# 
+#
 # # update grub config
 # # grub-mkconfig -o /boot/grub/grub.cfg
 # grub-mkconfig -o /boot/EFI/Linux/grub.cfg
