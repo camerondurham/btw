f5a7df421d8bf305994988d0e359495c7b29cd6d
Cameron Durham <camrs@protonmail.com>
2021-09-25 18:26:00 -0700

    Fix alias sourcing diff in zshrc

--- pkg/configure-system.sh
+++ pkg/configure-system.sh
@@ -15,15 +15,15 @@ localectl set-locale LANG=en_US.UTF-8
 
 
 # make vconsole font larger
-VCONSOLE_FONT=FONT=ter-v32n.psf.gz
-touch /etc/vconsole.conf
-grep -qxF ${VCONSOLE_FONT} /etc/vconsole.conf || sudo echo ${VCONSOLE_FONT} >> /etc/vconsole.conf
-
-# set grub font size
-grub-mkfont -s 60 -o /boot/grubfont.pf2 /usr/share/fonts/TTF/Hack-Regular.ttf
-GRUB_FONT=GRUB_FONT=\"/boot/grubfont.pf2\"
-grep -qxF ${GRUB_FONT} /etc/default/grub || echo ${GRUB_FONT} >> /etc/default/grub
-
-# update grub config
-# grub-mkconfig -o /boot/grub/grub.cfg
-grub-mkconfig -o /boot/EFI/Linux/grub.cfg
+# VCONSOLE_FONT=FONT=ter-v32n.psf.gz
+# touch /etc/vconsole.conf
+# grep -qxF ${VCONSOLE_FONT} /etc/vconsole.conf || sudo echo ${VCONSOLE_FONT} >> /etc/vconsole.conf
+# 
+# # set grub font size
+# grub-mkfont -s 60 -o /boot/grubfont.pf2 /usr/share/fonts/TTF/Hack-Regular.ttf
+# GRUB_FONT=GRUB_FONT=\"/boot/grubfont.pf2\"
+# grep -qxF ${GRUB_FONT} /etc/default/grub || echo ${GRUB_FONT} >> /etc/default/grub
+# 
+# # update grub config
+# # grub-mkconfig -o /boot/grub/grub.cfg
+# grub-mkconfig -o /boot/EFI/Linux/grub.cfg
